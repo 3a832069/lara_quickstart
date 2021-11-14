1b0523eac75cf9364e7703aca2555f850decdc6a
Len-En Chiu <linda34264622@gmail.com>
2021-11-14 21:48:15 +0800

    修正web.php的use App\Models\Task

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Models\Task;
 use Illuminate\Support\Facades\Route;
-use App\Task;
 use Illuminate\Http\Request;
 /*
 |--------------------------------------------------------------------------
